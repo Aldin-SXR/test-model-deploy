581995c5a529c676595b0716556e338aef42400b
Aldin Kovačević <aldin@salesseek.com>
2023-09-17 13:23:56 +0200

    Update

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once __DIR__.'/vendor/autoload.php';
 Flight::route('POST /analyze', function() {
     $request = Flight::request()->data->getData();
     $text = $request['text'];
-    putenv("PYTHONPATH=".__DIR__."/env/lib/python3.10/site-packages");
+    putenv("PYTHONPATH=".__DIR__."/env/lib/python3.9/site-packages");
 
     $result = exec(__DIR__.'/env/bin/python model/script.py "'. $text . '"');
     Flight::json([ 'message' => $result ]);
